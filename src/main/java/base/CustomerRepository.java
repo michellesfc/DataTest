9bb572531945500765df3368fc0b30587e50397d
michelle <michellesfc@gmail.com>
2017-02-28 19:34:05 -0800

    adding files

---
+++ src/main/java/base/CustomerRepository.java
@@ -0,0 +1,10 @@
+package base;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
+
+    List<Customer> findByLastName(String lastName);
+}
